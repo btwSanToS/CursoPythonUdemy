101b2022a9b607f6eec7ed840ba209fb428264f4
btwSanToS <andrelmsantos.87@gmail.com>
2026-05-07 20:07:46 -0300

    Aula 13 Tipo boolean

---
+++ tipo_boolean.py
@@ -0,0 +1,40 @@
+"""
+Tipo Boolean
+
+2 Constantes. Verdadeiro ou Falso
+True -> Verdadeiro
+False -> Falso
+
+OBS: Sempre com a inicial maiúscula.
+"""
+
+ativo = True
+print(ativo)
+
+"""
+Operações Básicas:
+"""
+# Negação (not):
+"""
+Fazendo a negação se o valor booleano for verdadeiro, o resultado
+será falso.
+Se for falso, será verdadeiro.
+Ou seja, sempre o contrário.
+"""
+print(not ativo)
+
+# Ou (or):
+"""
+É uma operação binária, ou seja, depende de dois ou mais valores.
+Ou um, ou outro deve ser verdadeiro.
+Caso exista uma expressão verdadeira, retornará True.
+"""
+
+# E (and):
+"""
+É uma operação binária, ou seja, depende de dois ou mais valores.
+Todos devem ser verdadeiros!!!!
+
+True and True --> True
+Caso tenha algum False, retornará False.
+"""
