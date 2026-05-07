932d7c16906c0712ae1c6b79c9e94dc57f31c0f9
btwSanToS <andrelmsantos.87@gmail.com>
2026-05-07 09:30:28 -0300

    tratando erro

---
+++ dir_help.py
@@ -0,0 +1,18 @@
+"""
+Utilitários Python para te auxiliar na programação.
+dir -> Apresenta todos os atributos/propriedades e funções/métodos
+disponíveis para determinado tipo de dado ou variável
+
+Ex: dir(tipo de dado/variável)
+dir("Geek")
+dir(4)
+
+help -> Apresenta a documentação/como utilizar os
+atributos/propriedades e funções/métodos disponíveis para
+determinado tipo de dado ou variável
+Ex: help("Geek".lower)
+"GEEK".lower() --> 'geek' // Tudo minúsculo
+"geek".upper() --> 'GEEK' // Tudo maiúsculo
+"university".title() --> 'University' // Primeira maiúscula
+num.__add__(6) --> 10
+"""
