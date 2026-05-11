adf07b4bc73d4095c18619599dc918de907dfe93
btwSanToS <andrelmsantos.87@gmail.com>
2026-05-11 15:13:03 -0300

    Aula 19 if, else, elif

---
+++ if_else_elif.py
@@ -0,0 +1,22 @@
+"""
+Estruturas condicionais
+if (se), else (se não), elif (else if)
+"""
+
+idade = 26
+
+"""
+if idade < 18:
+    print("Menor de Idade")
+else:
+    print("Maior de Idade")
+"""
+
+if idade > 18:
+    print("Maior de Idade!")
+elif idade == 18:
+    print("Tem 18 anos")
+elif idade == 26:
+    print("Tem 26 anos")
+else:
+    print("Menor de Idade")
