a497933b8eff9fb035e021e057b958bec79131a3
btwSanToS <andrelmsantos.87@gmail.com>
2026-05-11 15:35:51 -0300

    Aula 20 not, and, or, is

---
+++ 20_and_or_not_is.py
@@ -0,0 +1,40 @@
+"""
+Estruturas lógicas:
+and (e), or (ou), not (não), is (é)
+
+Operadores unários:
+    - not
+Operadores binários:
+    - and, or, is
+
+Para 'and', ambos os valores precisam ser True;
+Para 'or', um dos valores precisam ser True;
+Para 'not', o valor do booleano é invertido
+    True --> False
+    False --> True
+Para 'is' o valor é comparado com o segundo.
+"""
+ativo = False
+logado = False
+
+"""
+if ativo:
+    print("Usuário ativo no sistema!")
+else:
+    print("Usuário desativado!")
+    
+if ativo and logado:
+    print("Bem-vindo usuário!")
+else:
+    print("Você precisa ativar sua conta. Por favor, cheque seu e-mail.") 
+
+if ativo or logado:
+    print("Bem-vindo usuário!")
+else:
+    print("Você precisa ativar sua conta. Por favor, cheque seu e-mail.") 
+    
+if not ativo:
+    print("Você precisa ativar sua conta. Por favor, cheque seu e-mail.")
+else:
+    print("Seja bem-vindo!")
+"""
